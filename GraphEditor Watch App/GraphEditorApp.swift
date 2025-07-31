0dc97f2a70e6c20e1fa5bbcb6c8359cf044f4506
Aaron Bailey <aaron@handcart.io>
2025-07-31 11:58:18 -0700

    Initial Commit

---
+++ GraphEditor Watch App/GraphEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphEditorApp.swift
+//  GraphEditor Watch App
+//
+//  Created by handcart on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct GraphEditor_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
