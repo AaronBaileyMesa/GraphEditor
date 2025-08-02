2b06c6523a09288d6c5cae030ec0f579ee38f5bb
Aaron Bailey <aaron@handcart.io>
2025-08-01 22:17:46 -0700

    Refactor and recreation of the target

---
+++ GraphEditorWatchApp Watch App/GraphEditorWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphEditorWatchAppApp.swift
+//  GraphEditorWatchApp Watch App
+//
+//  Created by handcart on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct GraphEditorWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
